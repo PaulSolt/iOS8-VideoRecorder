f7f27a876421e05342c784dcfb7fdabb904bff38
Paul Solt <PaulSolt@gmail.com>
2019-10-02 13:12:36 -0400

    Choosing the best camera to use for our capture session

--- VideoRecorder/CameraViewController.swift
+++ VideoRecorder/CameraViewController.swift
@@ -7,17 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
 
+	lazy private var captureSession = AVCaptureSession()
+	
     @IBOutlet var recordButton: UIButton!
     @IBOutlet var cameraView: CameraPreviewView!
 
-
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+
+		// FUTURE: Choose between front / back cameras
+		setupCamera()
 	}
+	
+	private func setupCamera() {
+	
+		// Get camera
+		let camera = bestCamera()
+		
+		// Settings
+		
+		// Set the capture session on the cameraView
+	}
+	
+	private func bestCamera() -> AVCaptureDevice {
+		// Find a camera to use
+		
+		if let device = AVCaptureDevice.default(.builtInUltraWideCamera, for: .video, position: .back) {
+			return device
+		} else {
+			print("No ultra wide camera found on back")
+		}
+		
+		if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+			return device
+		}
+		
+		fatalError("No cameras on the device (or you're running in the simulator)")
+	}
+	
 
 
     @IBAction func recordButtonPressed(_ sender: Any) {
